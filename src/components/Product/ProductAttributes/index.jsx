2c4c528e7d032035f8cd5e7b089775b732968913
nilaysharma641 <nilaysharma641@gmail.com>
2021-04-25 13:32:05 +0530

    WYSIWYG methodology

--- src/components/Product/ProductAttributes/index.jsx
+++ src/components/Product/ProductAttributes/index.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Categories from './Categories'
 import BusinessModels from './BusinessModels'
 import Trl from './Trl'
-import { updateProducts } from 'store/actions/product'
+import { updateProducts, updateAtSource } from 'store/actions/product'
+import { GiSave } from 'react-icons/gi'
 
 const ProductAttributes = () => {
 	const dispatch = useDispatch()
 	const productDetails = useSelector((state) => state.product.productData)
+	const [isChanged, setIsChanged] = useState(false)
 
 	const handleUpdateProducts = (values, key) => {
 		productDetails[key] = values
 		dispatch(updateProducts(productDetails))
+		setIsChanged(true)
+	}
+
+	const handleUpdateAtSource = () => {
+		dispatch(updateAtSource(productDetails))
+		setIsChanged(false)
 	}
 
 	const renderCategories = () => {
@@ -47,6 +55,11 @@ const ProductAttributes = () => {
 			<div className='attributes-categories'>{renderCategories()}</div>
 			<div className='attributes-business-modals'>{renderBusinessModels()}</div>
 			<div className='attributes-trl'>{renderTrl()}</div>
+			{isChanged && (
+				<button onClick={handleUpdateAtSource} className='action-buttons'>
+					Update Changes <GiSave className='action-icons' />
+				</button>
+			)}
 		</div>
 	)
 }
--- src/store/actions/product.js
+++ src/store/actions/product.js
@@ -19,6 +19,16 @@ export const getProducts = (productId) => (dispatch) => {
 
 export const updateProducts = (product) => (dispatch) => {
 	dispatch({ type: 'SET_PRODUCT_LOADING' })
+	return dispatch({
+		type: 'GET_PRODUCT_DETAILS',
+		payload: {
+			productData: product
+		}
+	})
+}
+
+export const updateAtSource = (product) => (dispatch) => {
+	dispatch({ type: 'SET_PRODUCT_LOADING' })
 	fetch(`${process.env.REACT_APP_API_URL}/product/${product.id}/`, {
 		method: 'PUT',
 		headers: { 'Content-Type': 'application/json' },
